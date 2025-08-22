9f49238ff857b339c4e9a51f4264279c6861d72e
CahyaAji <cahya.akk@gmail.com>
2025-08-22 14:12:10 +0700

    simplify URL

--- src/lib/components/StatusWebv.svelte
+++ src/lib/components/StatusWebv.svelte
@@ -1,9 +1,13 @@
+<script>
+  import { API_URL } from "../utils/apihandler.js";
+</script>
+
 <div
   style="border-bottom: 2px solid white;background-color: #222222; color: white;"
 >
   <div>Status</div>
   <iframe
-    src="http://localhost:3000/config"
+    src={`${API_URL}/config`}
     title="Example website"
     style="width: 100%; height: 100%; border: none;"
   ></iframe>
