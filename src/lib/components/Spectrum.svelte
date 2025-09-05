c565537bca1fe27cd690aaac65fd98bc87f27806
CahyaAji <cahya.akk@gmail.com>
2025-09-05 23:11:12 +0700

    modify style

--- src/lib/components/Spectrum.svelte
+++ src/lib/components/Spectrum.svelte
@@ -1,3 +1,3 @@
 <div
-  style="background-color: #222222; width: 100%; height: 100%; border: 1px solid gray;"
+  style="background-color: #222222; width: 100%; height: 100%; border-top: 1px solid gray;"
 ></div>
