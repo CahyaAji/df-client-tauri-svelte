93f07709843b9b22d7ba5e657b95593f3f07908f
CahyaAji <cahya.akk@gmail.com>
2025-09-05 16:43:47 +0700

    add language format

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
+// Force US locale for the entire app
+if (typeof window !== "undefined") {
+  // Set the document language to US English
+  document.documentElement.lang = "en-US";
+
+  // Override navigator language properties
+  Object.defineProperty(navigator, "language", {
+    get: function () {
+      return "en-US";
+    },
+    configurable: true,
+  });
+
+  Object.defineProperty(navigator, "languages", {
+    get: function () {
+      return ["en-US"];
+    },
+    configurable: true,
+  });
+}
+
 const app = mount(App, {
   target: document.getElementById("app"),
 });
